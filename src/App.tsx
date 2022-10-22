d27310119fda732077d213bfcef1b23e1f743bef
Michal Prosniak <michalprosniak@gmail.com>
2022-10-22 13:40:56 +0200

    form with props, returning values, error messages - simple validation

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import './App.css';
 import { Form } from './Components/Form';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Form />
-      </header>
+      <h1>Search for project</h1>
+      <Form onSubmit={({name, owner, language}) => {
+        console.log(name, owner, language);
+      }}/>
+      
     </div>
   );
 }
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,36 +1,53 @@
-import React, { FormEvent } from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-export const Form: React.FC = () => {
+interface FormInputs {
+    name: string;
+    owner: string;
+    language: string;
+}
 
-    function submitForm(e: FormEvent)
-    {
-        e.preventDefault();
-    }
+interface Props {
+    onSubmit: (values: FormInputs) => void;
+}
+
+export const Form: React.FC<Props> = ({ onSubmit }) => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<FormInputs>();
 
     return (
-        <div>
-            <form onSubmit={submitForm} >
-                <label className='standard-label'>
+        <div className='form-wrapper'>
+            <form onSubmit={handleSubmit(onSubmit)} >
+                <label className='standard-label' htmlFor='name'>
                     Project name:
                 </label>
                 <input
-                    autoFocus
-                    required
+                    defaultValue=''
+                    {...register('name', { required: true })}
                     className='standard-input'
                     type='text'
                 ></input>
-                <label className='standard-label'>
+                {errors.name && <p className='error-message'>This field is required</p>}
+                <label className='standard-label' htmlFor='owner'>
                     Project owner:
                 </label>
                 <input
-                    required
+                    defaultValue=''
+                    {...register('owner', { required: true })}
                     className='standard-input'
                     type='text'
                 ></input>
-                <label className='standard-label'>
+                {errors.owner && <p className='error-message'>This field is required</p>}
+                <label className='standard-label' htmlFor='language'>
                     Language:
                 </label>
-                <select name='language' className='standard-input'>
+                <select 
+                className='standard-input'
+                {...register('language')}>
                     <option value='Java'>Java</option>
                     <option value='JavaScript'>JavaScript</option>
                     <option value='Go'>Go</option>
