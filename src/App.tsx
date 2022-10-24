457aa9c0e17a7b123b52df718a95600911754050
Michal Prosniak <michalprosniak@gmail.com>
2022-10-24 12:00:33 +0200

    refactor and bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import { Form } from './Components/Form';
 import { searchInApi, parametersCreator, dataParameters, dataResponse } from './ApiComunicator/SearchInApi';
@@ -15,52 +15,47 @@ function App() {
   const [searchedUser, setSearchedUser] = useState<string>('');
   const [searchedLanguage, setSearchedLanguage] = useState<string>('');
   const [maxPage, setMaxPage] = useState<number | undefined>(0);
-  let searchParams: dataParameters;
+  // let searchParams : dataParameters;
+  const searchParams = useRef<dataParameters>({q: ''});
+  
 
   useEffect( () => {
     const getData = async () => {
-      const res = await getSearchedData(searchedPhrase, searchedUser, searchedLanguage);
+      searchParams.current = parametersCreator(searchedPhrase, searchedUser, searchedLanguage, currentPage, itemsPerPage);
+      if (searchedPhrase !=='' && searchedUser !== '')
+      {
+        const res = await searchInApi(searchParams.current)
+        if(res?.status === 200)
+        {
+          setSearchResult(res);
+          setMaxPage(Math.ceil(res.data.total_count / itemsPerPage))
+          setIsError(false);
+        }
+        else{
+          setSearchResult(undefined)
+          console.log('error')
+          setIsError(true);
+        }
+      }
     }
     getData();
   }, [currentPage, searchedPhrase, searchedUser, searchedLanguage, itemsPerPage])
 
-  
 
-  async function getSearchedData(phrase: string, owner: string, language: string)
-  {
-    if (phrase !=='' && owner !== '')
-    {
-      searchParams = parametersCreator(phrase, owner, language, currentPage, itemsPerPage);
-      const res = await searchInApi(searchParams)
-      if(res?.status === 200)
-      {
-        setSearchResult(res);
-        setMaxPage(Math.ceil(res.data.total_count / itemsPerPage))
-        setIsError(false);
-      }
-      else{
-        setSearchResult(undefined)
-        console.log('error')
-        setIsError(true);
-        
-      }
-    }
-  }
-    
-  
   return (
     <div className="App">
       <h1>Search for project</h1>
       {maxPage && <h1>{maxPage}</h1>}
       <Form onSubmit={async ({phrase, owner, language}) => {
-        setSearchedLanguage(language)
-        setSearchedUser(owner)
-        setSearchedPhrase(phrase)
-        setCurrentPage(1)
+        setSearchedLanguage(language);
+        setSearchedUser(owner);
+        setSearchedPhrase(phrase);
+        setCurrentPage(1);
       }}/>
       {isError && <h1 className='error-message'>Something went wrong, please try again later!</h1>}
       {!isError && searchResult && <ResultsTable searchResult={searchResult} />}
       <button onClick={() => setCurrentPage((prevState) => prevState + 1)}>xxx</button>
+      <button onClick={() => setItemsPerPage((prevState) => prevState - 1)}>yyy</button>
 
     </div>
   );
